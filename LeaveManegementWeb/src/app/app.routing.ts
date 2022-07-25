a242ddf6071acd66bbbdd680ef0c5d1f6d36c86e
vamsi krishna <vamsi.n@edifybrokers.com>
2022-07-25 07:27:46 +0530

    added angular project

---
+++ LeaveManegementWeb/src/app/app.routing.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+import { FullComponent } from './layouts/full/full.component';
+
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      }
+    ]
+  }
+];
