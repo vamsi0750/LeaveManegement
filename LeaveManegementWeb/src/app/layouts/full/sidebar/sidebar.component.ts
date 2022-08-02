ceba8fd0b0ef4a93edad5085573039cf4099bdd0
vamsi krishna <vamsi.n@edifybrokers.com>
2022-08-02 23:05:50 +0530

    showing loggedin user name

--- LeaveManegementWeb/src/app/layouts/full/sidebar/sidebar.component.ts
+++ LeaveManegementWeb/src/app/layouts/full/sidebar/sidebar.component.ts
@@ -1,24 +1,29 @@
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MenuItems } from '../../../shared/menu-items/menu-items';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: []
 })
-export class AppSidebarComponent implements OnDestroy {
+export class AppSidebarComponent implements OnDestroy,OnInit {
   mobileQuery: MediaQueryList;
-
+  userInfo:any;
   private _mobileQueryListener: () => void;
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
     media: MediaMatcher,
-    public menuItems: MenuItems
+    public menuItems: MenuItems,
+    private auth:AuthService
   ) {
     this.mobileQuery = media.matchMedia('(min-width: 768px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+  ngOnInit(): void {
+   this.userInfo =  this.auth.getUserinfo();
   }
 
   ngOnDestroy(): void {
