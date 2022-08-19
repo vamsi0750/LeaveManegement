d0b2b1eb961849bb41d6f2c983f61e264c952e47
vamsi krishna <vamsi.n@edifybrokers.com>
2022-08-19 23:59:44 +0530

    form validation on signup

--- LeaveManegementWeb/src/app/shared/components/register/register.component.ts
+++ LeaveManegementWeb/src/app/shared/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { HelperService } from 'src/app/services/helper.service';
 import { LoginComponent } from '../login/login.component';
 
 LoginComponent
@@ -11,25 +12,29 @@ LoginComponent
 })
 export class RegisterComponent implements OnInit {
 
-  signupForm:FormGroup;
+  signupForm: FormGroup;
+  confirmPasswordMessage: boolean = false;
   constructor(
-    private fb:FormBuilder,
-    private auth:AuthService
-    ) { }
+    private fb: FormBuilder,
+    private auth: AuthService,
+    public helper: HelperService) { }
 
   ngOnInit(): void {
     this.signupForm = this.fb.group({
-      name:["",Validators.required],
-      email:["",Validators.email],
-      password:[""],
-      confirmPassword:[""]
+      name: ["", Validators.required],
+      email: ["", [Validators.email, Validators.required]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ["", [Validators.required, Validators.minLength(6)]]
     });
   }
 
-  signup(){
-    this.auth.register(this.signupForm.value).subscribe(res=>{
-      console.log(res);
-    });
+  signup() {
+    this.confirmPasswordMessage = false;
+    if (this.signupForm.value.password != this.signupForm.value.confirmPassword) {
+      this.confirmPasswordMessage = true;
+      return;
+    }
+    this.auth.register(this.signupForm.value).subscribe();
   }
 
 }
